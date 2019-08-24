d25f605223e2b55d707284d64c5fa555380a9012
staka121 <staka121@users.noreply.github.com>
2019-08-24 13:34:05 +0900

    fix extend class

--- src/drawer/draw_command.rb
+++ src/drawer/draw_command.rb
@@ -4,7 +4,7 @@ require_relative 'drawable'
 
 # ###
 # "シンボルの描画命令"を表現する
-class DrawCommand < Drawable
+class DrawCommand < Command
   def initialize(drawable, symbol)
     @drawable = drawable
     @symbol = symbol
